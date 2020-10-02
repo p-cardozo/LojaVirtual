830ee3028eebecd21a2b822ff06fc758874424af
Patricia Cardozo <cardozo.pa.ty@hotmail.com>
2020-10-02 20:13:01 -0300

    criando projeto

---
+++ LojaVirtual/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LojaVirtual
+//
+//  Created by Patricia dos Santos Cardozo on 02/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
